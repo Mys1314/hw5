c4839c71ee17181994cab7735f09c04f802a5106
Chengyuan Zhang <czhang23@usc.edu>
2025-04-23 04:50:00 +0000

    finished1

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+//function from wordle
+bool testSchedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row_day, int col_worker, vector<int>& ws);
+bool canBeSchedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row_day, int col_worker, vector<int>& ws);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -32,14 +34,56 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
+    sched = vector<vector<Worker_T>>(avail.size(), vector<Worker_T>(dailyNeed,0));
+    vector<int> workerSchedule(avail[0].size(),0);
     // Add your code below
+    return testSchedule(avail, dailyNeed, maxShifts, sched, 0, 0, workerSchedule);
+}
 
+bool testSchedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row_day, int col_worker, vector<int>& ws){
+  sched[row_day][col_worker] = 0;
+  //cout << sched[row_day][col_worker] << endl;
+  while((int)sched[row_day][col_worker] < (int)(avail[0].size())){
+    if(canBeSchedule(avail, dailyNeed, maxShifts, sched, row_day, col_worker, ws)){
+      ws[sched[row_day][col_worker]] += 1;
+      // cout << "here" << endl;
+      // cout << col_worker << " " << row_day<< " " << sched.size() << " "<<dailyNeed<<endl;
+      if(col_worker < (int)dailyNeed - 1){
+        if(testSchedule(avail, dailyNeed, maxShifts, sched, row_day, col_worker+1, ws)){
+          return true;
+        }else{
+          ws[sched[row_day][col_worker]] -= 1;
+          sched[row_day][col_worker] += 1;
+        }
+      }else if(row_day < (int)sched.size() - 1){
+        if(testSchedule(avail, dailyNeed, maxShifts, sched, row_day+1, 0, ws)){
+          return true;
+        }else{
+          ws[sched[row_day][col_worker]] -= 1;
+          sched[row_day][col_worker] += 1;
+        }
+      }else{
+        return true;
+      }
+    }else{
+      sched[row_day][col_worker] += 1;
+    }
+  }
+  sched[row_day][col_worker] = 0;
+  return false;
+}
 
-
-
+bool canBeSchedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row_day, int col_worker, vector<int>& ws){
+  if(avail[row_day][sched[row_day][col_worker]]==0){
+    return false;
+  }
+  if(ws[sched[row_day][col_worker]] == (int)maxShifts){
+    return false;
+  }
+  return true;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,8 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
+#include <stack>
 #endif
 
 #include "wordle.h"
@@ -13,7 +15,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool findNextWord(std::string& in, std::string floating, int p, std::set<std::string>& pwords,  const std::set<std::string>& dict, const std::string& original, int spaceleft);
+bool canbeword(std::string& in, std::string& floating, int p, const std::set<std::string>& dict, const std::string& original, int spaceleft, bool& found);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +25,97 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    stack<char> removed;
+    int spaceleft = 0;
+    for(int i = 0; i < (int)in.size(); i++){
+      if(in[i] == '-'){
+        spaceleft+=1;
+      }
+    }
+    std::string floatingc = floating;
+    std::string myw = in;
+    std::set<std::string> pwords;
+    int p = 0;
+    findNextWord(myw, floatingc, p, pwords, dict, in, spaceleft);
+    return pwords;
 
 }
 
 // Define any helper functions here
+bool findNextWord(std::string& in, std::string floating, int p, std::set<std::string>& pwords,  const std::set<std::string>& dict, const std::string& original, int spaceleft){
+    if((int)in.size() < 1){
+      return false;
+    }
+    //cout<<p<<endl;
+    //cout<<in<<endl;
+    bool found = false;
+    if(in[p] == '-'){
+      spaceleft -= 1;
+      if(spaceleft > (int)floating.size()-1){
+        for(in[p] = 'a'; in[p] <= 'z'; ++in[p]){
+          if(canbeword(in, floating, p, dict, original, spaceleft, found)){
+            if(p < (int)in.size() - 1){
+              findNextWord(in, floating, p+1, pwords, dict, original, spaceleft);
+            }else{
+              pwords.insert(in);
+            }
+          }
+          if(found){
+            floating += in[p];
+            found = false;
+          }
+        }
+      }else{
+        for(int i = 0; i < (int)floating.size(); i++){
+          in[p] = floating[i];
+          if(canbeword(in, floating, p, dict, original, spaceleft, found)){
+            if(p < (int)in.size() - 1){
+              findNextWord(in, floating, p+1, pwords, dict, original, spaceleft);
+            }else{
+              pwords.insert(in);
+            }
+          }
+          if(found){
+            floating += in[p];
+            found = false;
+          }
+        }
+      }
+      in[p] = '-';
+      return false;
+    }else{
+      if(p < (int)in.size() - 1){
+        return findNextWord(in, floating, p+1, pwords, dict, original, spaceleft);
+      }else{
+        if(dict.find(in) != dict.end()){
+          pwords.insert(in);
+        }
+        return false;
+      }
+    }
+}
+
+bool canbeword(std::string& in, std::string& floating, int p, const std::set<std::string>& dict, const std::string& original, int spaceleft, bool& found){
+    //cout<< in<<" "<<p<< endl;
+    if(original[p]=='-'){
+      for(int i = 0; i < (int)floating.size(); i++){
+        if(in[p] == floating[i]){
+          //cout<<"found "<<floating[i] << " " << in<< endl;
+          floating.erase(floating.begin()+i);
+          found = true;
+          break;
+        }
+      }
+    }
+    if((spaceleft - (int)floating.size()) >= 0){
+      //cout<<"cbw"<<endl;
+      if((int)in.size()-1 == p){
+        if(dict.find(in) != dict.end()){
+          return true;
+        }
+        return false;
+      }
+      return true;
+    }
+    return false;
+}
